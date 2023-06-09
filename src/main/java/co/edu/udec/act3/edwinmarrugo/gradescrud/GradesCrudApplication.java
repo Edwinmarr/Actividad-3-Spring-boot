76aee760259aa7d3a8a136a75a2e5990057ba5d8
Edwin Marrugo <edwinmarr@hotmail.com>
2023-06-08 19:53:23 -0500

    Inicialización del proyecto

---
+++ src/main/java/co/edu/udec/act3/edwinmarrugo/gradescrud/GradesCrudApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udec.act3.edwinmarrugo.gradescrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradesCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradesCrudApplication.class, args);
+	}
+
+}
